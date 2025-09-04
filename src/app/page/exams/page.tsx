22003bfe5549449af18d1528ff2bfe6bddabe471
kodenops <segun.femifadiya@gmail.com>
2025-09-04 20:00:45 +0100

    UPDATE: Adjusted button height and icon size for improved layout consistency

--- src/app/page/exams/page.tsx
+++ src/app/page/exams/page.tsx
@@ -82,10 +82,10 @@ const Page = () => {
 								key={mode}
 								onClick={() => handleNavigation(path)}
 								style={{ backgroundColor: colour }}
-								className='cursor-pointer h-[240px] md:w-[200px] w-[160px]  rounded-sm   font-bold text-[#696969]  relative px-2'>
+								className='cursor-pointer h-[200px] md:w-[200px] w-[160px]  rounded-sm   font-bold text-[#696969]  relative px-2'>
 								<div className=' h-full flex items-center justify-center flex-col w-full'>
 									<IconName
-										size={60}
+										size={40}
 										color='#fff'
 										className='mb-8'
 									/>
