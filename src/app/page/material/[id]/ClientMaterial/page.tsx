378df5f7b200691206fda16ea8acc172237f3a5e
kodenops <segun.femifadiya@gmail.com>
2026-02-05 10:14:11 +0100

    Refactor ClientMaterial component and update PageProps for improved structure

--- src/app/page/material/[id]/ClientMaterial/page.tsx
+++ src/app/page/material/[id]/ClientMaterial/page.tsx
@@ -1,37 +1,23 @@
-'use client';
+import ClientMaterial from './ClientMaterial';
 
-import dynamic from 'next/dynamic';
-import PptxViewer from '@/app/components/PptxViewer';
+type PageProps = {
+	params: {
+		id: string;
+	};
+	searchParams: {
+		type?: string;
+		title?: string;
+		author?: string;
+	};
+};
 
-const PdfViewer = dynamic(() => import('@/app/components/PdfViewer'), {
-	ssr: false,
-});
-
-export default function ClientMaterial({
-	materialId,
-	type,
-	title,
-	author,
-}: {
-	materialId: string;
-	type: string;
-	title: string;
-	author: string;
-}) {
+export default function Page({ params, searchParams }: PageProps) {
 	return (
-		<div className='p-6'>
-			<div className='my-2'>
-				<h1 className='text-2xl font-bold'>{title}</h1>
-				<h4 className='text-lg text-slate-300 italic'>
-					Author: {author.toWellFormed()}
-				</h4>
-			</div>
-			{type === 'pdf' && (
-				<PdfViewer fileUrl={`/api/material?id=${materialId}`} />
-			)}
-			{type === 'pptx' && (
-				<PptxViewer fileUrl={`/api/material?id=${materialId}`} />
-			)}
-		</div>
+		<ClientMaterial
+			materialId={params.id}
+			type={searchParams.type ?? ''}
+			title={searchParams.title ?? ''}
+			author={searchParams.author ?? ''}
+		/>
 	);
 }
