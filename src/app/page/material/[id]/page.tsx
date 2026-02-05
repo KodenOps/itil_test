4c83859c16fc1beaa0b5b0e2fc7fc737e37035bb
kodenops <segun.femifadiya@gmail.com>
2026-02-05 10:34:25 +0100

    Refactor Page component to simplify props typing by using 'any' for params and searchParams

--- src/app/page/material/[id]/page.tsx
+++ src/app/page/material/[id]/page.tsx
@@ -1,12 +1,6 @@
 import ClientMaterial, { ClientMaterialProps } from './ClientMaterial';
 
-export default function Page({
-	params,
-	searchParams,
-}: {
-	params: { id: string };
-	searchParams?: { type?: string; title?: string; author?: string };
-}) {
+export default function Page({ params, searchParams }: any) {
 	const { id } = params;
 	const { type, title, author } = searchParams || {};
 
